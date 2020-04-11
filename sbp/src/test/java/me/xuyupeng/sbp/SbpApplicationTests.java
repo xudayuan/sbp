caf2af03d252e0eb35f0cac67ed46f8b0a20fbda
xudayuan <xuypcumt@163.com>
2020-04-11 11:36:12 +0800

    启动

---
+++ sbp/src/test/java/me/xuyupeng/sbp/SbpApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xuyupeng.sbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
