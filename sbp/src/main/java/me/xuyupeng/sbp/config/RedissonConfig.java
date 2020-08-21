62762dc2e3ad6f30ba5e1fb39ffd8254165ace41
xudayuan <xuypcumt@163.com>
2020-08-21 20:02:29 +0800

    配置redisson

---
+++ sbp/src/main/java/me/xuyupeng/sbp/config/RedissonConfig.java
@@ -0,0 +1,49 @@
+package me.xuyupeng.sbp.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author xuyupeng
+ * @date 2020/8/21
+ */
+@Getter
+@Setter
+@Configuration
+public class RedissonConfig {
+    @Value("spring.redis.host")
+    private String host;
+
+    @Value("spring.redis.port")
+    private String port;
+
+    @Value("spring.redis.password")
+    private String passwd;
+
+    @Value("spring.redis.database")
+    private int database;
+
+    @Bean
+    public Config config() {
+        String address  = "redis://" + host + ":" + port;
+        Config config = new Config();
+        if (StringUtils.isEmpty(passwd)){
+        config.useSingleServer().setAddress(address).setDatabase(database);
+        }else {
+            config.useSingleServer().setAddress(address).setPassword(passwd).setDatabase(database);
+        }
+        return config;
+    }
+
+    @Bean
+    public RedissonClient redissonClient(Config config) {
+        return Redisson.create(config);
+    }
+}
